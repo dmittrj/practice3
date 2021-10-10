3a8e606159e6a3b60dad3cf7c40b6abcf2ea72c7
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-10-10 18:24:07 +0300

    Add files via upload

---
+++ Practice 1.3 p.2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_1._3_p._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        System.Windows.Forms.Timer t;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+            t = new System.Windows.Forms.Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();
+        }
+        void t_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 10;
+            if (progressBar1.Value >= 100) t.Stop();
+        }
+    }
+}
