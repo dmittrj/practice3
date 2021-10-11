978af1be8b5e10c82d7bcf027279bf2bb90211ad
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-10-11 18:00:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Practice_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ex. 1 
+            StreamWriter writer = File.CreateText(@"F:\newfile.txt");
+            writer.WriteLine("This is my new file");
+            writer.WriteLine("Do you like its format?");
+            writer.Close();
+            //ex. 2
+            StreamReader reader = File.OpenText(@"F:\newfile.txt");
+            string contents = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(contents);
+        }
+    }
+}
