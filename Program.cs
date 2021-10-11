44671f19c24490e64de6373c1407da9fd6544c8e
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-10-11 09:23:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1;
+            Int32 i32 = 1;
+            double db = 1;
+            i16 = 132;
+            i16 = db;
+            i32 = i16;
+            i32 = db;
+            db = 116;
+            db = i32;
+
+        }
+    }
+}
