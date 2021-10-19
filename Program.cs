b8b2424974adc65a486c31016142695adbd2f92a
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-10-19 17:45:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Practice_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"boot.ini");
+            StreamWriter sw = new StreamWriter("boot-utf7.txt", false, Encoding.UTF7);
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
