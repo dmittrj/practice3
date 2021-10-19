d4b875b692d3fcc9ba60dee7acb7260dba1f9ca1
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-10-19 18:26:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Practice_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myList = new ArrayList();
+            myList.Add("First");
+            myList.Add("Second");
+            myList.Add("Third");
+            myList.Add("Fourth");
+
+            foreach (string item in myList)
+            {
+                Console.WriteLine("Unsorted: {0}", item);
+            }
+
+            myList.Sort();
+
+            foreach (string item in myList)
+            {
+                Console.WriteLine("Sorted: {0}", item);
+            }
+        }
+    }
+}
