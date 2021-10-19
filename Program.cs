73de7505aa36f8ee51ee25a95885352b25626937
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-10-19 17:22:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace Practice_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ex. 1
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("Abcdefgh");
+            userWriter.Close();
+            //ex.2
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("There is no file to read");
+            } else
+            {
+                userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+                StreamReader userReader = new StreamReader(userStream);
+                string contents = userReader.ReadToEnd();
+                Console.WriteLine(contents);
+            }
+        }
+    }
+}
