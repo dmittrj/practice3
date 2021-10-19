fff9c78c641c187d8f7ca62407c8c2d63ead4d52
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-10-19 18:40:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Practice_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable lookup = new Hashtable();
+            lookup["0"] = "Zero";
+            lookup["1"] = "One";
+            lookup["2"] = "Two";
+            lookup["3"] = "Three";
+            lookup["4"] = "Four";
+            lookup["5"] = "Five";
+            lookup["6"] = "Six";
+            lookup["7"] = "Seven";
+            lookup["8"] = "Eight";
+            lookup["9"] = "Nine";
+
+            string ourNumber = "88005553535";
+
+            foreach (char c in ourNumber)
+            {
+                string digit = c.ToString();
+                if (lookup.ContainsKey(digit))
+                {
+                    Console.WriteLine(lookup[digit]);
+                }
+
+            }
+        }
+    }
+}
