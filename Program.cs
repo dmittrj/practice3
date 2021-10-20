bb5bb0c6a6f65243b20308f556a11e10e8f90de1
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-10-20 07:07:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace Practice_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+
+            list["Estados Unidos"] = "United Stated of America";
+            list["Canada"] = "Canada";
+            list["Espana"] = "Spain";
+
+            Console.WriteLine(list["espana"]);
+            Console.WriteLine(list["CANADA"]);
+        }
+    }
+}
