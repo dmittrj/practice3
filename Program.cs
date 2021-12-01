466ef83361a9dae4feee9011f963baef0e0f6cf5
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-12-01 19:09:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Practice_5._2
+{
+    class Person
+    {
+
+    }
+    class Program
+    {
+        private static void Serialize(Person sp)
+        {
+            FileStream fs = new FileStream("Person.XML", FileMode.Create);
+            XmlSerializer xs = new XmlSerializer(typeof(Person));
+            xs.Serialize(fs, sp);
+            fs.Close();
+        }
+
+        private static Person Deserialize()
+        {
+            Person dsp = new Person();
+            FileStream fs = new FileStream("Person.XML", FileMode.Open);
+            XmlSerializer xs = new XmlSerializer(typeof(Person));
+            dsp = (Person)xs.Deserialize(fs);
+            fs.Close();
+            return dsp;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
