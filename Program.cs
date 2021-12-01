f449d3c701dfc79b9268ad9fd0efaebfde8f4cb1
Dmitry Balabanov <91527966+dmittrj@users.noreply.github.com>
2021-12-01 19:06:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace P_5._1
+{
+    class Person
+    {
+
+    }
+    class Program
+    {
+        private static void Serialize(Person sp)
+        {
+            FileStream fs = new FileStream("Person.Dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(fs, sp);
+            fs.Close();
+        }
+        private static Person Deserialize()
+        {
+            Person dsp = new Person();
+            FileStream fs = new FileStream("Person.Dat", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            dsp = (Person) bf.Deserialize(fs);
+            fs.Close();
+            return dsp;
+        }
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
